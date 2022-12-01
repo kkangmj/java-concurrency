211c00e1419427bb81e4b557a4ea6e1c25ca8b38
minju <bbak0402dd@gmail.com>
2022-12-01 14:06:48 +0900

    Initial commit

---
+++ src/test/java/com/mangoo/javaconcurrency/JavaConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangoo.javaconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
